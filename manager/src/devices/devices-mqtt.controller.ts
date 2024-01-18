717120781663155f45f54b10f41fc64ed1d99f2b
n1md7 <b.kodua@gmail.com>
2024-01-18 16:25:56 +0400

    chore: remove retain flag once consumed

--- manager/src/devices/devices-mqtt.controller.ts
+++ manager/src/devices/devices-mqtt.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Inject, UseFilters, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Ctx, MessagePattern, Payload, RpcException } from '@nestjs/microservices';
+import { ClientMqtt, Ctx, MessagePattern, MqttRecordBuilder, Payload, RpcException } from '@nestjs/microservices';
 import { StatusReportMessage, StatusReportPayload } from '/src/devices/messages/status-report.message';
 import { RpcExceptionFilter } from '/libs/filters';
 import { DevicesService } from '/src/devices/devices.service';
@@ -11,6 +11,8 @@ import { FeedService } from '/src/feed/feed.service';
 import { ComponentsService } from '/src/components/components.service';
 import { DeviceStatus } from '/src/devices/enums/status.enum';
 import { validateSync } from 'class-validator';
+import { Client } from '/src/devices/enums/client.enum';
+import { DeviceOffMessage } from '/src/devices/types/device-control-message.type';
 
 @UseFilters(RpcExceptionFilter)
 @UseInterceptors(MessageIdInterceptor, MessageLoggerInterceptor, TimeoutInterceptor)
@@ -26,6 +28,7 @@ export class DevicesMqttController {
     @Inject(DevicesService) private readonly devicesService: DevicesService,
     @Inject(FeedService) private readonly feedService: FeedService,
     @Inject(ComponentsService) private readonly componentsService: ComponentsService,
+    @Inject(Client.DEVICES) private readonly client: ClientMqtt,
   ) {}
 
   @MessagePattern('home/devices/+/state')
@@ -37,11 +40,13 @@ export class DevicesMqttController {
 
     this.feedService.push(report);
 
+    if (report.status === DeviceStatus.ON) {
+      this.clearRetainedMessage(`home/devices/${report.code}/set`);
+    }
+
     await this.componentsService.updateManyByDeviceCode(report.code, {
       inUse: report.status === DeviceStatus.ON,
     });
-
-    // Todo Update inUse to false after duration
 
     await this.devicesService.softCreateDevice({
       code: report.code,
@@ -49,5 +54,9 @@ export class DevicesMqttController {
       name: report.name,
       version: report.version,
     });
+  }
+
+  private clearRetainedMessage(topic: string) {
+    return this.client.emit(topic, new MqttRecordBuilder().setQoS(1).setRetain(true).build());
   }
 }
