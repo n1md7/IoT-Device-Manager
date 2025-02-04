cfef5358390a656323d32530ac076bb32fefc39d
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-02-05 02:58:25 +0800

    fix css config issue.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './main.css';
+import './styles/main.css';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
