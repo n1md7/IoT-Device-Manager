6f9bcb02c24a7d1d910dbb166659a379a36d6d0d
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-02-17 04:10:16 +0800

    wip: system control views.

--- frontend/src/components/SystemControlView.tsx
+++ frontend/src/components/SystemControlView.tsx
@@ -4,68 +4,47 @@ const SystemControlView = () => {
       <div className="card-item">
         <div className="card-header">
           <div className="system-details">
-            <div className="system-name text-md">Water System</div>
-            <span className="uppercase">Off</span>
+            <div className="system-name text-green">Water System</div>
+            <span className="uppercase text-green">ON</span>
           </div>
           <div className="system-desc">A supply system for water tanks.</div>
         </div>
         <div className="card-body">
-          <div className="mb-8">
-            <label htmlFor="inputTimer" className="">Set timer: </label>
-            <input
-              list="datalist"
-              id="inputTimer"
-              type="range"
-              min="1"
-              max="20"
-              className="w-full"
-            />
-            <datalist id="datalist">
-              <option value="1"></option>
-              <option value="5"></option>
-              <option value="10"></option>
-              <option value="15"></option>
-              <option value="20"></option>
-            </datalist>
+          <div className="text-center">
+            <div className="mb-5">
+              <label htmlFor="remainingTime" className="block">Remaining time:</label>
+              <div className="mt-2">
+                <div className="input-group">
+                  <input type="text" value="14:53" name="remainingTime" id="remainingTime" className="input-field timer-count"
+                         placeholder="00:00" disabled/>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="text-center">Duration: 15 minutes</div>
         </div>
-        <div className="card-tool gap-3">
-          <button className="control-button">Turn ON</button>
+        <div className="card-tool">
+          <button className="control-button bg-green">Turn OFF</button>
           <button className="edit-button"></button>
         </div>
       </div>
       <div className="card-item">
         <div className="card-header">
           <div className="system-details">
-            <div className="system-name text-md">Water System</div>
-            <span className="uppercase">Off</span>
+            <div className="system-name text-orange">Water System</div>
+            <span className="uppercase">OFF</span>
           </div>
           <div className="system-desc">A supply system for water tanks.</div>
         </div>
         <div className="card-body">
           <div className="mb-8">
             <label htmlFor="inputTimer" className="">Set timer: </label>
-            <input
-              list="datalist"
-              id="inputTimer"
-              type="range"
-              min="1"
-              max="20"
-              className="w-full"
-            />
-            <datalist id="datalist">
-              <option value="1"></option>
-              <option value="5"></option>
-              <option value="10"></option>
-              <option value="15"></option>
-              <option value="20"></option>
-            </datalist>
+            <input id="inputTimer" type="range" className="input-range" />
           </div>
-          <div className="text-center">Duration: 15 minutes</div>
+          <div className="text-center">Total duration: 15 minutes</div>
         </div>
-        <div className="card-tool gap-3">
-          <button className="control-button">Turn ON</button>
+        <div className="card-tool">
+          <button className="control-button bg-orange">Turn ON</button>
           <button className="edit-button"></button>
         </div>
       </div>
