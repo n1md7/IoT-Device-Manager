2dabdca972bbef581d6eb1ff0eb39661e7a306fa
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-02-18 00:24:48 +0800

    added ui for main menu.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="h-full" id="mobile-menu">
-      <div className="flex flex-row justify-between">
-        <div className="nav-item bg-[url(nav.svg)]"></div>
-        <div className="nav-item bg-[url(home.svg)]"></div>
-        <div className="nav-item bg-[url(switch.svg)]"></div>
+    <>
+      <div className="h-full" id="mobile-menu">
+        <div className="flex flex-row justify-between">
+          <button className="nav-icon bg-[url(nav.svg)]" onClick={() => setIsMenuOpen(true)}></button>
+          <button className="nav-icon bg-[url(home.svg)]"></button>
+          <button className="nav-icon bg-[url(switch.svg)]"></button>
+        </div>
       </div>
-    </div>
+      {/*adding new system*/}
+      <div className={`${isMenuOpen ? "block" : "hidden"}`}>
+        <div className="menu-wrapper">
+          <div className="close">
+            <button className="nav-icon bg-[url(close.svg)]" onClick={() => setIsMenuOpen(false)}></button>
+          </div>
+          <div className="nav-items">
+            <button className="nav-item" onClick={() => console.log("manage systems clicked")}>Manage systems</button>
+            <button className="nav-item" onClick={() => console.log("component systems clicked")}>Manage components</button>
+            <button className="nav-item" onClick={() => console.log("report a bug clicked")}>Report a bug</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 export default NavBar;
