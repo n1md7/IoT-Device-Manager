d8097fc6db0623c1d27524ba3712ec37a709224c
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-02-20 05:03:00 +0800

    initialise api client

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -4,6 +4,7 @@ import Welcome from './Welcome.tsx';
 import AddNewSystem from './AddNewSystem.tsx';
 import AddNewComponent from './AddNewComponent.tsx';
 import { ReactElement, useEffect, useState } from 'react';
+import { api } from '../services/api-client.tsx';
 
 function App() {
   const [isNewSystemOpen, setIsNewSystemOpen] = useState(false);
@@ -13,64 +14,78 @@ function App() {
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Escape" && isNewSystemOpen) setIsNewSystemOpen(false);
-      if (event.key === "Escape" && isNewComponentOpen) setIsNewComponentOpen(false);
+      if (event.key === 'Escape' && isNewSystemOpen) setIsNewSystemOpen(false);
+      if (event.key === 'Escape' && isNewComponentOpen) setIsNewComponentOpen(false);
     };
     document.addEventListener('keydown', handleKeyDown);
-    return() => {
+    return () => {
       document.removeEventListener('keydown', handleKeyDown);
-    }
-  })
+    };
+  });
 
   const handleNewSystemView = () => {
     setIsNewSystemOpen(true);
     setShowModal(<AddNewSystem setIsNewSystemOpen={setIsNewSystemOpen} />);
-  }
+  };
 
   const handleNewComponentView = () => {
     setIsNewComponentOpen(true);
     setShowModal(<AddNewComponent setIsNewComponentOpen={setIsNewComponentOpen} />);
-  }
+  };
 
   const handleMenuClick = (component: ReactElement) => {
     setActiveView(component);
-  }
+  };
+
+  useEffect(() => {
+    api.devices
+      .get()
+      .then((response) => console.log(response.data.devices))
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    const { response, controller } = api.devices.getWithAbort();
+    response
+      .then((response) => {
+        console.log(response.data.devices);
+      })
+      .catch((error) => console.error(error));
+
+    return () => {
+      controller.abort('stop!');
+    };
+  }, []);
 
   return (
     <>
       <div className="container-main page-main">
         <div className="container-nav">
-          <NavBar onMenuClick={handleMenuClick}/>
+          <NavBar onMenuClick={handleMenuClick} />
         </div>
         <div className="container-header">
           <PageHeader />
         </div>
         <div className="container-body h-dvh">
-          <div className='page-body'>
-            {activeView}
-          </div>
+          <div className="page-body">{activeView}</div>
         </div>
         <div className="container-toolbar h-15">
           <div className="page-bottom">
-            <button className="button bg-purple text-white" onClick={handleNewSystemView}>Add System</button>
-            <button className="button bg-light-gray text-purple" onClick={handleNewComponentView}>Add Component</button>
+            <button className="button bg-purple text-white" onClick={handleNewSystemView}>
+              Add System
+            </button>
+            <button className="button bg-light-gray text-purple" onClick={handleNewComponentView}>
+              Add Component
+            </button>
           </div>
         </div>
       </div>
 
-      {isNewSystemOpen && (
-        <div className="block">
-          {showModal}
-        </div>
-      )}
+      {isNewSystemOpen && <div className="block">{showModal}</div>}
 
-      {isNewComponentOpen && (
-        <div className="block">
-          {showModal}
-        </div>
-      )}
+      {isNewComponentOpen && <div className="block">{showModal}</div>}
     </>
-  )
+  );
 }
 
 export default App;
--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -5,7 +5,7 @@ const Welcome = () => {
     <div className='welcome-container'>
       <div className="icon bg-[url(info.svg)]"><InfoIcon className={"text-light-dark"} /></div>
       <div className="header">Hi, Welcome!</div>
-      <div className='text'>Start adding by clicking "Add System" button below.</div>
+      <div className='text'>Please make sure you have at least one system configured. Start adding by clicking "Add System" button below.</div>
     </div>
   )
 }
