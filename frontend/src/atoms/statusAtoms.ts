180a40b6f780997cfd73894b5113b83702090f19
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-04-20 22:06:26 +0800

    deleted useless file.

--- frontend/src/atoms/statusAtoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'jotai';
-
-export const isFailedAtom = atom(false);
-export const isSuccessAtom = atom(false);
-export const errorDetailsAtom = atom<string | null>(null);
-export const errorMessageAtom = atom<string | null>(null);
