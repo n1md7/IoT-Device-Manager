c9c10fce52eff1fc227bd5a38b05d4c7b18d7bea
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-04-24 03:54:31 +0800

    fixed confirmation button style.

--- frontend/src/components/modals/Confirmation.tsx
+++ frontend/src/components/modals/Confirmation.tsx
@@ -16,14 +16,14 @@ const Confirmation = ({ action, cancel, itemToDelete }: Props) => {
               You are about to delete <em className="text-red-500 font-bold">{itemToDelete}</em> item!
             </span>
           </div>
-          <div className="button-container">
-            <button className="button bg-red-500 text-white mt-4 w-14" onClick={action}>
-              Delete
-            </button>
-            <button className="button bg-white text-light-dark mt-4 w-14" onClick={cancel}>
-              Cancel
-            </button>
-          </div>
+        </div>
+        <div className="button-container">
+          <button className="button hover:bg-red-600/80 bg-red-500 text-white mt-4" onClick={action}>
+            Delete
+          </button>
+          <button className="button cancel hover:text-light-gray/80 text-card-label mt-2 pb-0" onClick={cancel}>
+            Cancel
+          </button>
         </div>
       </div>
     </div>
